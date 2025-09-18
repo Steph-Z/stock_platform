400dbe588e5b543ad6c73af34026edc58a2241b0
Steph-Z <steph.zieger@gmail.com>
2025-09-18 22:15:57 +0200

    fix naming of plot after removing multicolumn representation

--- src/app/app.py
+++ src/app/app.py
@@ -14,22 +14,33 @@ app.title = "Stock Dashboard"
 
 #########Color settings
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+colors = {    'background': '#111111',
+        'input_background': "#7572728A",
+        'text': '#7FDBFF'
 }
 
 
 #We have to define the layout now
 
-app.layout = html.Div([html.H1('Stock Dashboard'),#as a header 
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1('Stock Dashboard',#as a header
+    style={
+            'textAlign': 'center',
+            'color': colors['text']
+        } 
+    ),
                       
     dcc.Input(
         id = 'Stockselection',
         placeholder = "Enter a Ticker or ISIN",
         type = 'text',
         value = 'AAPL',
-        debounce= True), 
+        debounce= True,
+        style={
+        'textAlign': 'center',
+        'color': colors['text'],
+        'backgroundColor': colors['input_background']
+        }), 
     
     #Store the data so I can acess it in multiple callbacks
     
@@ -61,7 +72,15 @@ def update_data_and_plot(ticker):
     data = yf.download(f'{ticker}', period = 'max')
     data.columns = data.columns.get_level_values(0) #get rid of the multi index for easier cashing
     data = data.reset_index()
+    data.index.name = 'Date'
+    #get the figure 
     fig = plot_stock_chart_line(data = data, ticker = ticker)
+    #change colors of the figure to match the layout
+    fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
    
     return  fig, data.to_dict('records')
 
@@ -78,6 +97,7 @@ def update_volatility(ticker, start_date, end_date, data_records):
     data = pd.DataFrame(data_records)
     data["Date"] = pd.to_datetime(data["Date"])
     data = data.set_index("Date")
+    data.index.name = 'Date'
     
     ticker = isin_ticker_to_ticker(ticker)
     vol, num_days = calculate_volatility((start_date, end_date), data, ticker)
--- src/utils/plots.py
+++ src/utils/plots.py
@@ -11,9 +11,9 @@ def plot_stock_chart_line(data: pd.DataFrame, ticker):
     comp_name = yf.Ticker(ticker).info['displayName']
     
     fig = px.line(data_frame= data,
-            x= data.index,
+            x= 'Date',
             y = 'Close',
-            labels = {ticker: y_name})
+            labels = {'Close': y_name, 'Date': data.index.name})
     
     fig.update_layout(title= {"text": f'Chart of the {comp_name} Stock', "font":{"size": 20}, "x": 0.5, "y":0.95, "xanchor": "center", "yanchor": "top"})
     
