f4214b5db59d4c5ebc17b987cab07d9fc5870ed9
Steph-Z <steph.zieger@gmail.com>
2025-09-21 18:27:11 +0200

    update misc colors

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,5 +1,5 @@
 
-
+#not used anymore since using bootstrap layouts now
 colors = {    'background': "#FFFFFF",
         'input_background': "#FFFFFFFF", #some input boxes cant handle a different color so white is a kind of given to avoid a lot of custom code (i dont fancy doing css here)
         'chart_background': "#ffffff",
