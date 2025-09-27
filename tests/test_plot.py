9964179bbdb1c83ccc203378cb8133c607ba08c6
Steph-Z <steph.zieger@gmail.com>
2025-09-27 20:16:25 +0200

    update plot test for metadata usage, new metadata input

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -12,16 +12,11 @@ def test_plot_stock_chart_line_returns_figure():
     })
     comp_name = 'Apple' #is passed from main app usually, always will be there 
     #cause we catch wrong tickers/isins earlier
+    
+    #fake metadata 
+    metadata = {'currency': 'USD'}
 
+    fig = plot_stock_chart(df, comp_name, 'AAPL', chart_type= 'line', metadata= metadata )
 
-
-    # Mock yfinance.Ticker.info
-    with patch('yfinance.Ticker') as MockTicker:
-        MockTicker.return_value.info = {
-            'currency': 'USD'
-        }
-
-        fig = plot_stock_chart(df, comp_name, 'AAPL', chart_type= 'line')
-
-        assert isinstance(fig, Figure)
-        assert fig.data[0].y.tolist() == [100, 102, 101, 99, 98]
+    assert isinstance(fig, Figure)
+    assert fig.data[0].y.tolist() == [100, 102, 101, 99, 98]
